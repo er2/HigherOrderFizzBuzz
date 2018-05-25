6b08363f1fd87363ff686cb457a9c59bd2ad8180
Eric Riese <eric.riese@gmail.com>
2018-05-25 12:17:12 -0400

    dedup

--- src/FBL.java
+++ src/FBL.java
@@ -1,30 +1,39 @@
+import java.util.function.IntPredicate;
 import java.util.function.UnaryOperator;
 import java.util.stream.IntStream;
 
-public class FBL {
+public class FBL
+{
 
-   public static void main(String[] args) {
+   public static void main(String[] args)
+   {
       IntStream.rangeClosed(1, 100)
            .mapToObj(FBL::fizzbuzz)
            .forEach(System.out::println);
    }
 
-   private static String fizzbuzz(int n) {
-      UnaryOperator<UnaryOperator<String>> fizz = f -> {
-         if (n % 3 == 0) {
-            return g -> "Fizz" + f.apply("");
-         } else {
-            return f;
+   @FunctionalInterface
+   interface StageGenerator
+   {
+      UnaryOperator<String> generate(IntPredicate test, String output, UnaryOperator<String> func);
+   }
+
+   private static String fizzbuzz(int n)
+   {
+      StageGenerator sg = (test, output, func) -> {
+         if (test.test(n))
+         {
+            return g -> output + func.apply("");
+         }
+         else
+         {
+            return func;
          }
       };
-      UnaryOperator<UnaryOperator<String>> buzz = f -> {
-         if (n % 5 == 0) {
-            return g -> "Buzz" + f.apply("");
-         } else {
-            return f;
-         }
-      };
-      return fizz.apply(buzz.apply(f -> f)).apply(Integer.toString(n));
+      UnaryOperator<UnaryOperator<String>> fizz = f -> sg.generate(k -> k % 3 == 0, "Fizz", f);
+      UnaryOperator<UnaryOperator<String>> buzz = f -> sg.generate(k -> k % 5 == 0, "Buzz", f);
+      UnaryOperator<String> id = s -> s;
+      return fizz.apply(buzz.apply(id)).apply(Integer.toString(n));
    }
 
 }
